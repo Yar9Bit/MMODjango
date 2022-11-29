dddb0ad694acfcf2550d674d6d80cd92942bfff7
yar9bit <yar9bit@gmail.com>
2022-11-29 22:36:55 +0500

    1

--- board/apps.py
+++ board/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MmoConfig(AppConfig):
+class BoardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mmo'
+    name = 'board'
